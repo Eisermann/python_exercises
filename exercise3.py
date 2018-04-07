c9119c5a3807cafe409e47fb97150d4dc32c303c
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2018-04-07 15:29:10 +0200

    push

---
+++ exercise3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+def function(list=[]):
+    list.append('test')
+    print(list)
+
+function()
+function()
+function()
+function(['some other value'])
+function()
+function()
