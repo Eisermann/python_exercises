e8843845f89d6ea9bf13c30155a02e1a9d083074
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2018-04-07 12:18:43 +0200

    Add exercise with flatteing, unique values and sets

---
+++ exercise1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+workers = [
+    {
+        'name': 'Piotr',
+        'surname': 'Kowalski',
+        'city': 'Warszawa',
+    }, 
+    {
+        'name': 'Julian',
+        'surname': 'Wysoki',
+        'city': 'Krakow',
+    },
+    {
+        'name': 'Andrzej',
+        'surname': 'Niski',
+        'city': 'Warszawa',
+    },
+]
+
+unique_cities = None
+
+# unique_cities = ...
+
+print(unique_cities)
