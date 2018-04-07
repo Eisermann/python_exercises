6009a61fbfad36520c96c9b5ccdc5f5d90e59935
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2018-04-07 13:56:08 +0200

    Add pdb exercise

---
+++ exercise2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import random
+
+def random_value():
+    return random.randint(0, 10)
+
+test_var = {
+    'test': random_value()
+}
+
+#import pdb; pdb.set_trace()
+
+print ('too late')
